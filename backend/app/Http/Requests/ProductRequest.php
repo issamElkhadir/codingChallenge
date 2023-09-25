dfb632f7ce5dbb21fe6cbd6425f3d077cc27562f
issamElkhader <issamelkhader55@gmail.com>
2023-09-25 12:24:00 +0100

    Centralize Validation Error Handling in ProductRequest

--- backend/app/Http/Requests/ProductRequest.php
+++ backend/app/Http/Requests/ProductRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ProductRequest extends FormRequest
 {
@@ -30,5 +32,13 @@ class ProductRequest extends FormRequest
             'image' => 'nullable' ,
             'categories' => 'required|array'
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => $validator->errors(),
+        ]));
     }
 }
